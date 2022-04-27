3936a654eb1e0c667225f99b801542cdcb6eca24
tobiasvmj <tobiasvmj@gmail.com>
2022-04-27 10:53:01 -0300

    Update LuckyController.php

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController extends AbstractController
+class LuckyController
 {
     
     @Route("/lucky/number")
